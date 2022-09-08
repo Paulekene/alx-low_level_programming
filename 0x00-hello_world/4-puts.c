abad7833f181937c9ed13d0b8b399064fd332b94
paulekene <commathtechnology@live.co>
2022-09-08 05:54:08 -0700

    Task 4: Output using puts command

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints tis clause "Programming is like building a multilingual puzzle
+ * Return: 0 if no syntax erro but non-zero if otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
