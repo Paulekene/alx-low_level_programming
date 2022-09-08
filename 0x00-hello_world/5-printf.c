b6588a67387049bb27dcc4e5fdac083da7d735d4
paulekene <commathtechnology@live.co>
2022-09-08 07:59:48 -0700

    Task 5 Betty correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main – Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -10,4 +10,3 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
