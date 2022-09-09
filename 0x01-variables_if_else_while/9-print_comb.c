949cea509e89fd60788100794220064eb102e02a
paulekene <commathtechnology@live.co>
2022-09-09 14:12:47 -0700

    Approved

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 /**
- * main - Prints reversed alphabet.
- *
- * Return: Always 0 (Success)
+ * main- Write a program that prints all possible
+ * combinations of single-digit numbers
+ * Return: 0 (success)
  */
+
 int main(void)
 {
-	int i;
+	int d;
 
-	for (i = 122; i > 96; i--)
+	for (d = '0'; d <= '9'; d++)
 	{
-		putchar(i);
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
