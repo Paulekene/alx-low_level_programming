e3c16e0543bab5b599720fdd9be6d537e14da25c
paulekene <commathtechnology@live.co>
2022-09-09 13:08:11 -0700

    Task 3; Using Betty standard

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,33 +1,19 @@
 #include <stdio.h>
 
 /**
-
-* main - Prints the alphabet.
-
-*
-
-* Return: Always 0 (Success)
-
-*/
-
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	char alx[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-char alx[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-int i;
-
-for (i = 0; i < 52; i++)
-
-{
-
-putchar(alx[i]);
-
-}
-
-putchar('\n');
-
-return (0);
-
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alx[i]);
+	}
+	putchar('\n');
+	return (0);
 }
