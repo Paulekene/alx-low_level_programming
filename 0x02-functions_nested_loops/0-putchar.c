da96daff4461862124a039148049de38ed985a33
paulekene <commathtechnology@live.co>
2022-09-13 11:14:56 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,14 @@
  */
 int main(void)
 {
-    char ab = '_putchar';
-    _putchar(ab);
-    return (0);
+	_putchar('_');
+    	_putchar('p');
+    	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+    	return (0);
 }
